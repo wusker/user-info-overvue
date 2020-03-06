be0474bd71551babdf5aeebfb6227232fa66ed28
wusker <s.bergellio@gmail.com>
2020-03-05 17:03:59 -0700

    Init

--- README.md
+++ README.md
@@ -1,24 +1,13 @@
-# overvue
+# Overvue of User Info
 
 ## Project setup
+
 ```
 yarn install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 yarn serve
 ```
-
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>User Info</h1>
+    <hr />
+    <div class="userInfo">
+      <span>Location: {{userInfo.location}}</span>
+      <span>Time Zone: UTC-{{userInfo.timeZone}}</span>
+      <span>Seconds on Page: {{userInfo.timeOnPage}}</span>
+      <span>Current Page: {{userInfo.page}}</span>
+      <span>Screen: {{userInfo.screenHeight}} x {{userInfo.screenWidth}}</span>
+      <span>DPR: {{userInfo.dpr}}</span>
+      <span>Browser Name: {{userInfo.browserName}}</span>
+      <span>Browser Engine: {{userInfo.browserEngine}}</span>
+      <span>Pages: {{userInfo.previous}}</span>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      userInfo: {
+        dpr: devicePixelRatio,
+        screenHeight: screen.height,
+        screenWidth: screen.width,
+        location: "",
+        timeZone: new Date().getTimezoneOffset() / 60,
+        timeOnPage: 0,
+        page: window.location.pathname,
+        browserName: navigator.appName,
+        browserEngine: navigator.product,
+        previous: history.length
+      }
+    };
+  },
+  mounted() {
+    this.getUserLocation();
+
+    setInterval(() => {
+      this.startTimer();
+    }, 1000);
+  },
+  methods: {
+    async getUserLocation() {
+      const url = "http://ip-api.com/json/";
+      const response = await fetch(url);
+      const result = await response.json();
+      const { regionName, city } = result;
+      this.userInfo.location = `${city}, ${regionName}`;
+    },
+    startTimer() {
+      this.userInfo.timeOnPage += 1;
+    }
   }
-}
+};
 </script>
 
 <style>
+body {
+  background: #05050c;
+  display: flex;
+  justify-content: center;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, system-ui, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #00d9ff;
   margin-top: 60px;
+}
+hr {
+  margin: 3vw 0vw;
+  border-color: #00d9ff;
+}
+.userInfo {
+  display: flex;
+  flex-direction: column;
+}
+.userInfo span {
+  font-size: 1.5rem;
+  font-weight: 500;
+  margin-bottom: 0.5rem;
 }
 </style>
